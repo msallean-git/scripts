f0a500a28973084e59621ad4d5462621fe530864
msallean-git <58023224+msallean-git@users.noreply.github.com>
2020-02-01 01:53:19 -0600

    Initial Commit of Data Script and run_local
    Initial project upload

---
+++ api_helpers/dsjwt.rb
@@ -0,0 +1,17 @@
+require 'jwt'
+require 'OpenSSL'
+
+rsa_private = OpenSSL::PKey::RSA.new(ARGV[2])
+rsa_public = rsa_private.public_key
+payload = {iss: ARGV[0], sub: ARGV[1], iat: 1, exp: 2147483647, aud: "account-d.docusign.com", scope: "signature impersonation"}
+token = JWT.encode payload, rsa_private, 'RS256'
+
+puts token
+
+decoded_token = JWT.decode token, rsa_public, true, { algorithm: 'RS256' }
+
+puts decoded_token
+#response =  RestClient.post('https://account-d.docusign.com/oauth/token?', headers: {grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer', assertion: token})
+#response = RestClient.post('https://account-d.docusign.com/oauth/token', {grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer', assertion: token})
+#response = RestClient::Request.execute(method: :post, url: 'https://account-d.docusign.com/oauth/token', headers: {grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer', assertion: token})
+#puts response.body
---
+++ run_local.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+branch=$1
+directory=$2
+script=$3
+script_args="${*:4}"
+
+set -e
+runtime=`date +%Y%m%d_%H%M%S`
+if [ -d scripts ]; then cd scripts && git fetch -q origin && git checkout $branch && git pull origin $branch; else git clone -q -b $branch https://github.com/msallean-git/scripts.git data_scripts && cd scripts && git checkout -q $branch; fi
+sloc="$directory/$script"
+if [ ! -f $sloc ]; then echo 'Cannot find script at $sloc' && exit 1; fi
+script_cmd="ruby $sloc"
+echo "Running: $script_cmd $script_args"
+$script_cmd $script_args
