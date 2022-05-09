d84c96eb39d05087f007e9db86e0c6256fb5729f
mae.sallean <mae.sallean@maesallean.local>
2022-05-09 09:42:33 -0500

    Show uri_str

--- api/adobesign_cred.rb
+++ api/adobesign_cred.rb
@@ -10,9 +10,10 @@ puts "client_secret -> #{ARGV[2]}"
 puts "code -> #{ARGV[3]}"
 puts "====================================================================="
 
-#access_token = ""
-#refresh_token = ""
-#uri_str = "https://secure.#{ARGV[0]}.adobesign.com/oauth/v2/token"
+access_token = ""
+refresh_token = ""
+uri_str = "https://secure.#{ARGV[0]}.adobesign.com/oauth/v2/token"
+puts uri_str
 #RestClient.post(uri_str, {grant_type: :authorization_code, :code => "#{ARGV[3]}", :client_id => "#{ARGV[1]}", :client_secret => "#{ARGV[2]}", :redirect_uri => "https://www.onit.com" }){|response, request, result, &block|
 #	case response.code
 #	when 200
