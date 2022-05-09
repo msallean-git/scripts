9dceb717f339986df7d133cf27fb100cd039f309
mae.sallean <mae.sallean@maesallean.local>
2022-05-09 10:28:58 -0500

    hardcoded

--- api/adobesign_cred.rb
+++ api/adobesign_cred.rb
@@ -13,7 +13,8 @@ puts "====================================================================="
 access_token = ""
 refresh_token = ""
 uri_str = "https://secure.#{ARGV[0]}.adobesign.com/oauth/v2/token"
-RestClient.post(uri_str, payload: {grant_type: "authorization_code", code: "#{ARGV[3]}", client_id: "#{ARGV[1]}", client_secret: "#{ARGV[2]}", redirect_uri: "https://www.onit.com"}){|response, request, result, &block|
+payload_str = 
+RestClient.post(uri_str, '{“grant_type":"authorization_code","code":"CBNCKBAAHBCAABAAASHJlb2THiBylNom-E7qsvN5qZ9h8g7P","client_id":"CBJCHBCAABAA7STPKahkmRhOX5Vz28EZPCR-RCjC2GDA","client_secret":"y2XSoK0wWqCYYX5K6EAqvwaVgRPmBOfp","redirect_uri":"https://www.onit.com”}'){|response, request, result, &block|
 	case response.code
 	when 200
 		puts "200: Valid Response"
