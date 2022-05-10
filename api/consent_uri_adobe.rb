0ed50cf529bfed96b0af88c878b2f2b6db4619fe
mae.sallean <mae.sallean@maesallean.local>
2022-05-10 09:40:42 -0500

    String

--- api/consent_uri_adobe.rb
+++ api/consent_uri_adobe.rb
@@ -9,4 +9,4 @@ puts "client_id -> #{ARGV[1]}"
 puts "====================================================================="
 
 
-puts "https://secure.#{ARGV[0]}.adobesign.com/public/oauth/v2?redirect_uri=https://www.onit.com&response_type=code&client_id=#{ARGV[1]}&scope=user_login:account+agreement_read:account+agreement_write:account+agreement_send:account+library_read:account+library_write:account+workflow_read:account+workflow_write:account+webhook_read:account+webhook_write:account:webhook_retention:account
+puts "https://secure.#{ARGV[0]}.adobesign.com/public/oauth/v2?redirect_uri=https://www.onit.com&response_type=code&client_id=#{ARGV[1]}&scope=user_login:account+agreement_read:account+agreement_write:account+agreement_send:account+library_read:account+library_write:account+workflow_read:account+workflow_write:account+webhook_read:account+webhook_write:account:webhook_retention:account"
